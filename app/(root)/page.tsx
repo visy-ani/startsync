d02862037ebefa91eed8eba8c68ab2a1e5f4b516
chocoanish <chocoboyanish566@gmail.com>
2024-12-31 00:53:16 +0530

    Add utility functions, components, and Tailwind CSS configuration; implement search functionality using Nest Js Form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,15 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({searchParams}: {
+  searchParams: Promise<{query?: string}>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
       <section className="pink_container">
         <h1 className="heading">Pitch Your Startup, <br /> Connect with Entrepreneurs</h1>
         <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
+        <SearchForm query={query}/>
       </section>
     </>
   );
