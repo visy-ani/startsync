8662c1d3dd19ac8641d2c486f734d9ba573dd6a6
chocoanish <chocoboyanish566@gmail.com>
2024-12-31 07:16:30 +0530

    Enhance startup query to support search functionality; update data fetching in Home component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,7 +10,8 @@ export default async function Home({searchParams}: {
   searchParams: Promise<{query?: string}>;
 }) {
   const query = (await searchParams).query;
-  const { data: posts } = await sanityFetch({query: STARTUPS_QUERY})
+  const params = { search: query || null }
+  const { data: posts } = await sanityFetch({query: STARTUPS_QUERY, params})
 
   return (
     <>
