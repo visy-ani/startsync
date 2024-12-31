5a69536c1124ba11eb9d8844d8731ee7514156bc
chocoanish <chocoboyanish566@gmail.com>
2024-12-31 23:35:12 +0530

    Implement view tracking; add write client for updating view count in the database

--- components/View.tsx
+++ components/View.tsx
@@ -1,15 +1,24 @@
 import Ping from "@/components/Ping";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
+import { after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
   try {
-    const data = await client
+    const {views: totalViews} = await client
       .withConfig({ useCdn: false })
       .fetch(STARTUP_VIEWS_QUERY, { id });
-      console.log(data)
+      
 
-    const totalViews = data?.views || 0;
+    after(
+      async () =>{
+        await writeClient
+          .patch(id)
+          .set({views: totalViews + 1})
+          .commit()
+      }
+    );
 
     return (
       <div className="view-container">
