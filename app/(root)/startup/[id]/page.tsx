d2270702a520ff9209ecac4bb71aba5eaeeda9fd
chocoanish <chocoboyanish566@gmail.com>
2025-01-01 14:32:53 +0530

    Add editor picks section and playlist schema; implement parallel fetching for startup details

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { formatDate } from "@/lib/utils";
 import { client } from "@/sanity/lib/client";
-import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { PLAYLIST_BY_SLUG_QUERY, STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
@@ -8,14 +8,23 @@ import markdownit from "markdown-it";
 import { Suspense } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
 const md = markdownit();
 
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+// Parallel Fetching
+  const [post, editorPosts] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, {id}),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {slug: 'editor-picks'})
+  ]);
+
+
   if (!post) return notFound();
   const parsedContent = md.render(post?.pitch || '');
+  
 
   return (
     <>
@@ -67,7 +76,17 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
         <hr className="divider" />
 
-        {/* { TODO: EDITOR SELECTED STARTUPS} */}
+        {editorPosts.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor Picks</p>
+            
+            <ul className="mt-7 card_grid-sm">
+                {editorPosts.map((post: StartupTypeCard, index: number) =>
+                 <StartupCard key={index} post={post}/>
+                )}
+            </ul>
+          </div>
+        )}
 
         <Suspense fallback={<Skeleton className="view-skeleton"/>} >
           <View id={id}/>
